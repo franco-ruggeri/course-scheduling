71b3dbe1a5903e7ca07ad4bce4ee3f4ff2933863
ryared <ryanyared@gmail.com>
2019-10-14 17:30:17 +0200

    added isValid

--- src/solvers/genetic/Genetic.java
+++ src/solvers/genetic/Genetic.java
@@ -1,5 +1,16 @@
 package solvers.genetic;
 
+import generator.Problem;
+
 public class Genetic {
+    final Problem p;
+
+    public Genetic(Problem p) {
+        this.p = p;
+    }
+
+    private void mutate(final int[][] a){
+
+    }
 
 }
