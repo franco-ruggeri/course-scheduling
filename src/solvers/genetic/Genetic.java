aefc5086bf8b1a002044b9472406b84eb5bffb21
Franco Ruggeri <fruggeri@kth.se>
2019-10-15 16:24:05 +0200

    complete GA, not tested

--- src/solvers/genetic/Genetic.java
+++ src/solvers/genetic/Genetic.java
@@ -4,7 +4,6 @@ import java.util.LinkedList;
 import java.util.List;
 import java.util.Random;
 
-import generator.Evaluator;
 import generator.Problem;
 import generator.Solution;
 
@@ -13,7 +12,6 @@ import generator.Solution;
  * chapter 4 of "Artificial Intelligence, a modern approach - Russell, Norvig".
  */
 public class Genetic {
-    private final Problem problem;
     private final int populationSize;
     private List<Individual> population;
     private double mutationProbability;
@@ -21,67 +19,29 @@ public class Genetic {
     private final long maxTime;
     private Random random;
     
-    private class Individual {
-    	String genes;
-    	int fitnessValue;
-    	
-    	Individual(Solution state) {
-			/*
-			 * A state is a matrix of courses (rows and columns are timeslots and
-			 * classrooms, respectively). To represent it as sequence of genes, we need
-			 * #Timeslots x #Classrooms x log2(#Courses) bits.
-			 */
-    		int courseCount = problem.getCourseCount();
-            int timeslotCount = problem.getTimeslotsCount();
-            int classrooomCount = problem.getClassroomCount();
-            int[][] schedule = state.getSolution();
-            StringBuffer sb = new StringBuffer();
-    		for (int t=0; t<timeslotCount; t++)
-    			for (int cl=0; cl<classrooomCount; cl++)
-    				for (int c=0; c<courseCount; c++)
-    					sb.append(Integer.valueOf(schedule[t][cl]).toString());
-    		// TODO non e' completo l'encoding, in questo modo ho #timeslots x #classrooms x 32 bit
-    		genes = sb.toString();
-    	}
-    	
-    	Individual(String genes) {
-    		this.genes = genes;
-    		this.fitnessValue = fitness(genes);
-		}
-
-    }
-    
 	public Genetic(Problem problem, int populationSize, double mutationProbability, int enoughFitness, long maxTime) {
-        this.problem = problem;
         this.populationSize = populationSize;
         this.mutationProbability = mutationProbability;
         this.enoughFitness = enoughFitness;
         this.maxTime = maxTime;
         this.random = new Random();
         
-        // init population with random states
+        // init population with random states (complete representation of states!)
         population = new LinkedList<>();
-        int courseCount = problem.getCourseCount();
-        int timeslotCount = problem.getTimeslotsCount();
-        int classrooomCount = problem.getClassroomCount();
-        for (int i=0; i<populationSize; i++) {
-			int[][] schedule = new int[timeslotCount][classrooomCount];
-			for (int t=0; t<timeslotCount; t++)
-				for (int cl=0; cl<classrooomCount; cl++)
-					schedule[t][cl] = random.nextInt(courseCount);
-			population.add(new Individual(new Solution(schedule)));
-        }
+        for (int i=0; i<populationSize; i++)
+			population.add(new Individual(problem));
     }
 
 	public Solution simulate() {
 		Individual bestIndividual = null;
+		int maxFitnessValue;
 		double startTime = System.currentTimeMillis();
 		
     	do {
     		// evolution
-	    	List<String> newPopulation = new LinkedList<>();
+	    	List<Individual> newPopulation = new LinkedList<>();
 	    	for (int i=0; i<populationSize; i++) {
-	    		String child = reproduce(select(), select());
+	    		Individual child = reproduce(select(), select());
 	    		
 				/*
 				 * Mutation should happen with a certain probability. In order to obtain this,
@@ -89,57 +49,39 @@ public class Genetic {
 				 * random variable X~U(0,1), F(x) = P(X<=x) = x.
 				 */
 	    		if (random.nextDouble() <= mutationProbability)
-	    			child = mutate(child);
+	    			child.mutate();
 	    		newPopulation.add(child);
 	    	}
 	    	
 	    	// update population
-	    	population.clear();
-			newPopulation.forEach(i -> population.add(new Individual(i)));
+	    	population = new LinkedList<>(newPopulation);
 			
 			// get best individual
-			int maxFitnessValue = population.stream().mapToInt(i -> i.fitnessValue).max().getAsInt();
-	    	bestIndividual = population.stream().filter(i -> i.fitnessValue == maxFitnessValue).findFirst().get();
+			int aux = population.stream().mapToInt(Individual::getFitnessValue).max().getAsInt();
+	    	bestIndividual = population.stream().filter(i -> i.getFitnessValue() == aux).findFirst().get();
+	    	maxFitnessValue = aux;	// aux is used because closures require effective final variables
 	    	
 	    	// terminate when time runs out or when a good-enough individual has been found
-    	} while (System.currentTimeMillis() - startTime < maxTime && bestIndividual.fitnessValue < enoughFitness);
+		} while (System.currentTimeMillis() - startTime < maxTime && maxFitnessValue < enoughFitness);
     	
-	    return genesToState(bestIndividual.genes);
+	    return bestIndividual.getSolution();
     }
 
-	private String select() {
+	private Individual select() {
 		/*
 		 * Fitness proportionate selection (roulette-wheel selection):
 		 * 1. sum all fitness values -> s
-		 * 2. generate random value in (0, S) -> r
+		 * 2. generate random value in (0, s) -> r
 		 * 3. go through the population summing the fitness values and stop when the sum
 		 *  is greater than r
 		 */
-		int s = population.stream().mapToInt(i -> i.fitnessValue).sum();
-		int r = random.nextInt(s);
-		return population.stream().filter(i -> i.fitnessValue > r).findFirst().get().genes;
+		int s = population.stream().mapToInt(Individual::getFitnessValue).sum();
+		int r = random.nextInt(s+1);
+		return population.stream().filter(i -> i.getFitnessValue() > r).findFirst().get();
 	}
 	
-	private String reproduce(String x, String y) {
-		int n = x.length();
-		int c = random.nextInt(n);	// random crossover point
-		return x.substring(0, c) + y.substring(c+1, n);
-	}
-	
-	private String mutate(String genes) {
-		int n = genes.length();
-		int m = random.nextInt(n);						// index of mutating gene
-		char g = genes.charAt(m) == '0' ? '1' : '0';	// mutated gene
-		return genes.substring(0, m-1) + g + genes.substring(m+1, n);
-	}
-	
-	int fitness(String genes) {
-		return Evaluator.evaluate(problem, genesToState(genes));
-	}
-	
-	private Solution genesToState(String genes) {
-		// TODO
-		return null;
+	private Individual reproduce(Individual x, Individual y) {
+		return new Individual(x, y);
 	}
 	
 }
