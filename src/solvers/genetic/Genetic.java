707996768d000970505ee2eb9c035fb23ce138fe
Franco Ruggeri <fruggeri@kth.se>
2019-10-15 02:53:37 +0200

    add genetic algorithm, almost completed
    representation of state as string of genes is to be completed

--- src/solvers/genetic/Genetic.java
+++ src/solvers/genetic/Genetic.java
@@ -1,28 +1,143 @@
 package solvers.genetic;
 
+import java.util.LinkedList;
+import java.util.List;
+import java.util.Random;
+import java.util.function.Function;
+
 import generator.Problem;
+import generator.Solution;
 
+/**
+ * Implementation of genetic algorithms (GA) following the pseudo-code in
+ * chapter 4 of "Artificial Intelligence, a modern approach - Russell, Norvig".
+ */
 public class Genetic {
-    final Problem p;
+    private final Problem problem;
+    private final Function<String, Integer> fitnessFunction;
+    private int populationSize;
+    private List<Individual> population;
+    private double mutationProbability;
+    private int enoughFitness;
+    private long maxTime;
+    private Random random;
+    
+    private class Individual {
+    	String genes;
+    	int fitnessValue;
+    	
+		/*
+		 * A state is a matrix of courses (rows and columns are timeslots and
+		 * classrooms, respectively). To represent it as sequence of genes, we need
+		 * #Timeslots x #Classrooms x log2(#Courses) bits.
+		 */
+    	Individual(Solution state) {
+    		int courseCount = problem.getCourseCount();
+            int timeslotCount = problem.getTimeslotsCount();
+            int classrooomCount = problem.getClassroomCount();
+            
+    		for (int t=0; t<timeslotCount; t++) {
+    			for (int cl=0; cl<classrooomCount; cl++) {
+    				for (int c=0; c<courseCount; c++) {
+    					
+    				}
+    			}
+    		}
+    	}
+    	
+    	Individual(String genes) {
+    		this.genes = genes;
+    		this.fitnessValue = fitnessFunction.apply(genes);
+		}
 
-    public Genetic(Problem p) {
-        this.p = p;
+    }
+    
+	public Genetic(Problem problem, Function<Solution, Integer> fitnessFunction, int populationSize,
+			double mutationProbability, int enoughFitness, long maxTime) {
+        this.problem = problem;
+        this.fitnessFunction = s -> fitnessFunction.apply(genesToState(s));
+        this.populationSize = populationSize;
+        this.mutationProbability = mutationProbability;
+        this.enoughFitness = enoughFitness;
+        this.maxTime = maxTime;
+        this.random = new Random();
+        
+        // init population with random states
+        population = new LinkedList<>();
+        int courseCount = problem.getCourseCount();
+        int timeslotCount = problem.getTimeslotsCount();
+        int classrooomCount = problem.getClassroomCount();
+        for (int i=0; i<populationSize; i++) {
+			int[][] schedule = new int[timeslotCount][classrooomCount];
+			for (int t=0; t<timeslotCount; t++)
+				for (int cl=0; cl<classrooomCount; cl++)
+					schedule[t][cl] = random.nextInt(courseCount);
+			population.add(new Individual(new Solution(schedule)));
+        }
     }
 
-    private int[][][] initPop(){
-        return null;
+	public Solution simulate() {
+		Individual bestIndividual = null;
+		double startTime = System.currentTimeMillis();
+		
+    	do {
+    		// evolution
+	    	List<String> newPopulation = new LinkedList<>();
+	    	for (int i=0; i<populationSize; i++) {
+	    		String child = reproduce(select(), select());
+	    		
+				/*
+				 * Mutation should happen with a certain probability. In order to obtain this,
+				 * we extract a random value with uniform distribution (0,1). Recall that for a
+				 * random variable X~U(0,1), F(x) = P(X<=x) = x.
+				 */
+	    		if (random.nextDouble() <= mutationProbability)
+	    			child = mutate(child);
+	    		newPopulation.add(child);
+	    	}
+	    	
+	    	// update population
+	    	population.clear();
+			newPopulation.forEach(i -> population.add(new Individual(i)));
+			
+			// get best individual
+			int maxFitnessValue = population.stream().mapToInt(i -> i.fitnessValue).max().getAsInt();
+	    	bestIndividual = population.stream().filter(i -> i.fitnessValue == maxFitnessValue).findFirst().get();
+	    	
+	    	// terminate when time runs out or when a good-enough individual has been found
+    	} while (System.currentTimeMillis() - startTime < maxTime && bestIndividual.fitnessValue < enoughFitness);
+    	
+	    return genesToState(bestIndividual.genes);
     }
 
-    private void mutate(final int[][] a){
-
-    }
-
-    private int[][][] crossOver(final int[][] a, final int[][] b){
-        return null;
-    }
-
-    private int[][][] select(final int[][][] pop) {
-        return null;
-    }
-
+	private String select() {
+		/*
+		 * Fitness proportionate selection (roulette-wheel selection):
+		 * 1. sum all fitness values -> s
+		 * 2. generate random value in (0, S) -> r
+		 * 3. go through the population summing the fitness values and stop when the sum
+		 *  is greater than r
+		 */
+		int s = population.stream().mapToInt(i -> i.fitnessValue).sum();
+		int r = random.nextInt(s);
+		return population.stream().filter(i -> i.fitnessValue > r).findFirst().get().genes;
+	}
+	
+	private String reproduce(String x, String y) {
+		int n = x.length();
+		int c = random.nextInt(n);	// random crossover point
+		return x.substring(0, c) + y.substring(c+1, n);
+	}
+	
+	private String mutate(String genes) {
+		int n = genes.length();
+		int m = random.nextInt(n);	// index of mutating gene
+		return genes.substring(0, m-1) + random.nextInt(1) + genes.substring(m+1, n);
+	}
+	
+	private Solution genesToState(String genes) {
+		// TODO
+		return null;
+	}
+	
 }
